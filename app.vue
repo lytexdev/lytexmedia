c7b4e0207efbdcd4f07a597b2d50287239515893
Immanuel Mruk <immanuel.mruk@8mylez.com>
2024-07-12 09:56:28 +0200

    Improve Styling

--- app.vue
+++ app.vue
@@ -5,12 +5,22 @@
         <div class="container">
             <b>Website ist aktuell im Aufbau! ;)</b>
 
-            <img src="https://media1.tenor.com/m/7Qff-aO5G0QAAAAd/genba-neko.gif" alt="Cats" height="256px">
+            <img @click="showAlert()"
+                title="Aktull im Aufbau!"
+                src="https://media1.tenor.com/m/7Qff-aO5G0QAAAAd/genba-neko.gif"
+                alt="Builder cats"
+            >
         </div>
     </main>
 
     <Footer />
 </template>
+
+<script setup lang="ts">
+const showAlert = () => {
+    alert('Wir sind noch am Bauen!')
+}
+</script>
 
 <style lang="scss" scoped>
     #content {
@@ -20,6 +30,12 @@
         .container {
             font-size: 22px;
             font-weight: 400;
+        }
+
+        img {
+            margin-top: 2.5rem;
+            max-height: 328px;
+            max-width: 100%;
         }
     }
 </style>
--- components/Footer.vue
+++ components/Footer.vue
@@ -1,29 +1,22 @@
 <template>
-    <footer class="footer">
+    <footer>
         <div class="container">
             <div class="footer-content">
                 <div class="footer-section links">
-                    <h2>Quick Links</h2>
+                    <h3>Quick Links</h3>
                     <ul>
-                        <li>
-                            <NuxtLink to="/">Home</NuxtLink>
-                        </li>
-                        <li>
-                            <NuxtLink to="/services">Services</NuxtLink>
-                        </li>
-                        <li>
-                            <NuxtLink to="/linkleap">LinkLeap</NuxtLink>
-                        </li>
-                        <li>
-                            <NuxtLink to="/contact">Contact</NuxtLink>
-                        </li>
+                        <li><NuxtLink to="/">Home</NuxtLink></li>
+                        <li><NuxtLink to="/services">Services</NuxtLink></li>
+                        <li><NuxtLink to="/linkleap">LinkLeap</NuxtLink></li>
+                        <li><NuxtLink to="/coconut">Coconut</NuxtLink></li>
+                        <li><NuxtLink to="/contact">Kontakt</NuxtLink></li>
                     </ul>
                 </div>
 
                 <div class="footer-section social">
-                    <h2>Folge uns</h2>
+                    <h3>Folge uns</h3>
                     <div class="social-icons">
-                        <a aria-label="Instagram" href="https://www.instagram.com/lytexmedia/" target="_blank">
+                        <a aria-label="lytexmedia Instagram" title="lytexmedia Instagram" href="https://www.instagram.com/lytexmedia/" target="_blank">
                             <svg width="48px" height="48px" viewBox="0 0 32 32" fill="none"
                                 xmlns="http://www.w3.org/2000/svg">
                                 <rect x="2" y="2" width="28" height="28" rx="6" fill="url(#paint0_radial_87_7153)" />
@@ -64,11 +57,31 @@
                                 </defs>
                             </svg>
                         </a>
+                        <a arial-label="ximmanuel Github" title="ximmanuel Github" href="https://github.com/ximmanuel" target="_blank">
+                            <svg width="48px" height="48px" viewBox="0 0 20 20" version="1.1"
+                                xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                                <title>github [#142]</title>
+                                <desc>Created with Sketch.</desc>
+                                <defs>
+                                </defs>
+                                <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                    <g id="Dribbble-Light-Preview" transform="translate(-140.000000, -7559.000000)"
+                                        fill="#fff">
+                                        <g id="icons" transform="translate(56.000000, 160.000000)">
+                                            <path
+                                                d="M94,7399 C99.523,7399 104,7403.59 104,7409.253 C104,7413.782 101.138,7417.624 97.167,7418.981 C96.66,7419.082 96.48,7418.762 96.48,7418.489 C96.48,7418.151 96.492,7417.047 96.492,7415.675 C96.492,7414.719 96.172,7414.095 95.813,7413.777 C98.04,7413.523 100.38,7412.656 100.38,7408.718 C100.38,7407.598 99.992,7406.684 99.35,7405.966 C99.454,7405.707 99.797,7404.664 99.252,7403.252 C99.252,7403.252 98.414,7402.977 96.505,7404.303 C95.706,7404.076 94.85,7403.962 94,7403.958 C93.15,7403.962 92.295,7404.076 91.497,7404.303 C89.586,7402.977 88.746,7403.252 88.746,7403.252 C88.203,7404.664 88.546,7405.707 88.649,7405.966 C88.01,7406.684 87.619,7407.598 87.619,7408.718 C87.619,7412.646 89.954,7413.526 92.175,7413.785 C91.889,7414.041 91.63,7414.493 91.54,7415.156 C90.97,7415.418 89.522,7415.871 88.63,7414.304 C88.63,7414.304 88.101,7413.319 87.097,7413.247 C87.097,7413.247 86.122,7413.234 87.029,7413.87 C87.029,7413.87 87.684,7414.185 88.139,7415.37 C88.139,7415.37 88.726,7417.2 91.508,7416.58 C91.513,7417.437 91.522,7418.245 91.522,7418.489 C91.522,7418.76 91.338,7419.077 90.839,7418.982 C86.865,7417.627 84,7413.783 84,7409.253 C84,7403.59 88.478,7399 94,7399"
+                                                id="github-[#142]">
+                                            </path>
+                                        </g>
+                                    </g>
+                                </g>
+                            </svg>
+                        </a>
                     </div>
                 </div>
 
                 <div class="footer-section contact">
-                    <h2>Contact</h2>
+                    <h3>Kontakt</h3>
                     <p>Email: <a href="mailto:contact@lytexmedia.com">contact@lytexmedia.com</a></p>
                     <p>Discord: <a href="https://discord.com/users/510708544425951233" target="_blank">_lytex_</a></p>
                 </div>
@@ -77,12 +90,8 @@
             <div class="footer-bottom">
                 <p>&copy; 2024 by lytexmedia</p>
                 <ul>
-                    <li>
-                        <NuxtLink to="/impressum">Impressum</NuxtLink>
-                    </li>
-                    <li>
-                        <NuxtLink to="/datenschutz">Datenschutz</NuxtLink>
-                    </li>
+                    <li><NuxtLink to="/impressum">Impressum</NuxtLink></li>
+                    <li><NuxtLink to="/datenschutz">Datenschutz</NuxtLink></li>
                 </ul>
             </div>
         </div>
