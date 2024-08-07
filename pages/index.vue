edd9cf67296edde781f4cff3caea0d3a7aa0a845
Immanuel Mruk <immanuel.mruk@8mylez.com>
2024-08-07 10:00:24 +0200

    Update startpage image

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
             <b>Website ist aktuell im Aufbau!</b>
             <b><br>Klick dich am besten in der Navigation rum ;)</b>
 
-            <img title="Aktuell im Aufbau!" src="/images/programming-cat.webp" alt="Aktuell im Aufbau!" loading="lazy">
+            <img title="Aktuell im Aufbau!" src="/images/programming-seal.jpg" alt="Aktuell im Aufbau!" loading="lazy">
         </div>
     </main>
 </template>
